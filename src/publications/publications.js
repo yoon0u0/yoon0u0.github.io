7ebb541aa8bc12d79afa581e713598fc4dc84924
yoon0u0 <yoon0u0@gmail.com>
2025-01-17 14:49:15 +0900

    update proxona acceptance

--- src/publications/publications.js
+++ src/publications/publications.js
@@ -44,7 +44,7 @@ function Publications() {
                             <div className="author"><mark>Yoonseo Choi</mark>, Eun Jeong Kang, Seulgi Choi, Min Kyung Lee, Juho Kim</div>
                             <div className="conf-link">
                                 <div className="conf">
-                                    <div className='conf-content'>arXiv</div>
+                                    <div className='conf-content'>CHI 2025 (Conditionally Accepted)</div>
                                 </div>
                                 <div className="paper-link">
                                     <a className = "each-link" href = "https://arxiv.org/abs/2408.10937">arXiv</a> 
